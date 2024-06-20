afc43befb160968db5c44762130052b2092ec0ac
Nawid-A <123211442+Nawid-A@users.noreply.github.com>
2024-06-19 23:49:49 -0400

    overhauled initial layout of homepage, added three new sections. Section 1 is the welcome interface, section 2 displays transit data (backend data and API to be implemented later), and section 3 is a to-do list/calendar

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var icon= document.getElementById('weather-icon');  
-const currentSRC= String(icon.getAttribute('src'));  
-const newSRC= String(currentSRC.replace('static', 'animated'));
- 
-function hover(){  
-    icon.setAttribute('src', newSRC); 
-} 
-function unhover(){ 
-    icon.setAttribute('src', currentSRC);
+/*let bird = document.getElementById('bird');
+let flyRight = true;
+
+function toggleFlyDirection() {
+    if (flyRight) {
+        bird.style.animation = 'flyRightToLeftAndFlap 5s linear forwards'; // Fly from right to left with flapping
+    } else {
+        bird.style.animation = 'flyLeftToRightAndFlap 5s linear forwards'; // Fly from left to right with flapping
+    }
+    flyRight = !flyRight; // Toggle the direction for the next interval
+
+    // Call the function again to switch directions after the current animation ends (5000ms or 5s)
+    setTimeout(toggleFlyDirection, 5000);
 }
+
+toggleFlyDirection(); // Start the animation when the page loads
+*/
